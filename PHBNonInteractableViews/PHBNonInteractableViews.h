4b83a2b715bc4466046133c476edc71df12b36fa
phlippieb <phlippie.bosman@gmail.com>
2020-04-11 11:29:40 +0200

    Initial Commit

---
+++ PHBNonInteractableViews/PHBNonInteractableViews.h
@@ -0,0 +1,19 @@
+//
+//  PHBNonInteractableViews.h
+//  PHBNonInteractableViews
+//
+//  Created by Phlippie Bosman on 2020/04/11.
+//  Copyright © 2020 Phlippie Bosman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PHBNonInteractableViews.
+FOUNDATION_EXPORT double PHBNonInteractableViewsVersionNumber;
+
+//! Project version string for PHBNonInteractableViews.
+FOUNDATION_EXPORT const unsigned char PHBNonInteractableViewsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PHBNonInteractableViews/PublicHeader.h>
+
+
